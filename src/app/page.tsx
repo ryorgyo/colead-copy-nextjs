13f6f87de7bf1c5c6b9484866f94e709a95aba7a
ryorgyo <ryorgyo@icloud.com>
2023-01-24 11:55:32 +0900

    💡 contact form 一段階目まで完成

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -66,6 +66,27 @@ export default function Home() {
       a: "価格の調整が入る可能性がありますが、買取可能です。",
     },
   ];
+
+  const FORMITEMS = [
+    {
+      title: "直近1年の売電金額（税込）",
+      name: "sale",
+      essential: true,
+      unit: true,
+    },
+    {
+      title: "土地賃料",
+      name: "rent",
+      essential: true,
+      unit: true,
+    },
+    {
+      title: "稼働開始年月",
+      name: "period",
+      essential: true,
+      unit: true,
+    },
+  ];
   return (
     <div>
       <main className={styles.main}>
@@ -267,14 +288,31 @@ export default function Home() {
             <h2>無料金額シミュレーション</h2>
           </div>
           <div className={styles.contactBox}>
-            <div className={styles.one}>
-              <h3>お持ちの発電所の情報を教えてください。</h3>
-              <div className={styles.formBox}>
-                <p>直近1年の売電金額（税込）</p>
-                <p>必須</p>
-                <input type="number" name="number" id="number" />
-                <p>万円</p>
-              </div>
+            <h3>お持ちの発電所の情報を教えてください。</h3>
+            {FORMITEMS.map((item) => {
+              return (
+                <div className={styles.formBox} key={item.title}>
+                  <div className={styles.left}>
+                    <p className={styles.title}>{item.title}</p>
+                    {item.essential ? (
+                      <p className={styles.essential}>必須</p>
+                    ) : null}
+                  </div>
+                  <div className={styles.right}>
+                    <input type="number" name={item.name} id={item.name} />
+                    {item.essential ? <p>万円</p> : null}
+                  </div>
+                </div>
+              );
+            })}
+            <div className={styles.btn}>
+              <Image
+                src={`/contact/start-btn.png`}
+                alt={""}
+                fill
+                sizes=""
+                className={styles.flowImg}
+              />
             </div>
           </div>
         </section>
